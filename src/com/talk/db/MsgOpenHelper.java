03c4c45644a7ad2bdfa3262f53ebf1d748ebd34b
vencial <venciallee@gmail.com>
2013-04-28 23:04:48 +0800

    code Message Database helper.

--- src/com/talk/db/MsgOpenHelper.java
+++ src/com/talk/db/MsgOpenHelper.java
@@ -2,28 +2,29 @@ package com.talk.db;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class MsgOpenHelper extends SQLiteOpenHelper{
 
 	private static final int DATABASE_VERSION = 2;
-	private static final String MSG_TABLE_NAME = "message";
+	private static final String DATABASE_NAME = "talkIm";
+	public static final String MSG_TABLE_NAME = "MESSAGE";
+	public static final String FROM_COL = "FROM";
+	public static final String MSG_COL = "MSG";
+	private static final String MSG_TABLE_CREATE = "CREATE TABLE " + MSG_TABLE_NAME + FROM_COL +" TEXT, " + MSG_COL + " TEXT);";
 	
-	public MsgOpenHelper(Context context, String name, CursorFactory factory,
-			int version) {
-		super(context, name, factory, version);
+	public MsgOpenHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		
+		db.execSQL(MSG_TABLE_CREATE);
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		
 	}
-	
 	
 }
